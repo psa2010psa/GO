04d535f1b6bc47fe8eaf975289056878a13d3966
peng chen <peng.chen@gaeamobile.com>
2021-03-04 14:57:05 +0800

    request info

--- examples/9_http编程/04_http服务器.go
+++ examples/9_http编程/04_http服务器.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 //w 给客户端回复数据
 //r 读取客户端发送的数据
-func HandConn(w http.ResponseWriter, req *http.Request) {
+func HandConn(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("r.Method = ", r.Method)
+	fmt.Println("r.URL = ", r.URL)
+	fmt.Println("r.Header = ", r.Header)
+	fmt.Println("r.Body = ", r.Body)
+
 	w.Write([]byte("hello go")) //给客户端回复数据
 }
 
@@ -18,6 +24,13 @@ func main() {
 	http.ListenAndServe(":8000", nil)
 
 	/*浏览器访问 http://127.0.0.1:8000/mike.html
-	结果 hello go
+	  浏览器显示结果 hello go
+	*/
+
+	/*服务器端打印结果
+	r.Method =  GET
+	r.URL =  /mike.html
+	r.Header =  map[Accept:[text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng]]
+	r.Body =  {}
 	*/
 }
